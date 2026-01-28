3713276879fda4a1150274fecf40ffb4a8a9353e
Israel Palma <ipalm001@gmail.com>
2026-01-28 11:39:36 -0300

    Mi primer projecto java

---
+++ DemoJavaApp/src/com/alkemy/demo/Main.java
@@ -0,0 +1,15 @@
+package com.alkemy.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// escribir en consola
+		System.out.println("Hola clase!!");
+		//System.err.println("Esto es un error");
+		System.out.println();
+		System.out.print("Main.main()\n");
+		System.out.print(0);
+		System.out.print("otro texto");
+	}
+
+}

95cdfba880623ec5e0221e6dcf7b0341ac2dba91
Israel Palma <ipalm001@gmail.com>
2026-02-04 10:08:55 -0300

    VAriables Locales y Variables de Clase

---
+++ DemoJavaApp/src/com/alkemy/f20260204/Main.java
@@ -0,0 +1,42 @@
+package com.alkemy.f20260204;
+
+public class Main {
+	//variable de clase
+	static Integer variableClase;
+	
+	public static void main(String[] parametros) {
+		// variable local
+		int variableLocal = 0;
+		variableClase = 1;
+		
+		System.out.println("*** main ***");
+		System.out.println(variableLocal);//0
+		variableLocal=6;
+		
+		System.out.println(variableClase);//1
+		
+		//llamado a la funcion
+		imprimir();
+		imprimir2(variableLocal);//imprimir2(6)
+		
+		System.out.println("*** main2 ***");
+		System.out.println(variableLocal);
+		
+		imprimir2(5);
+	}
+	
+	public static void imprimir() {
+		System.out.println("*** imprimir ***");
+		//System.out.println(variableLocal);//alcance
+		System.out.println(variableClase);//1
+		variableClase = 3;//modificar o reasignar un valor
+	}
+	
+	public static void imprimir2(int varLocal) {
+		int varLocarImprimir2 = 4;
+		System.out.println("*** imprimir 2 ***");
+		System.out.println(varLocal);//6;5
+		System.out.println(variableClase);//3
+		varLocal = 2;
+	}
+}
---
+++ SmartTask/src/cl/palma/Main.java
@@ -0,0 +1,15 @@
+package cl.palma;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	static String[] tareas;
+	static ArrayList<String> tasks;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!!");
+	}
+
+}
